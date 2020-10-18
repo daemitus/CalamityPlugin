3fb5f7837dc79ac35b82db4822e75022d9a6224e
Raymond Lynch <raymond.lynch@ironnetcybersecurity.com>
2020-10-18 09:13:18 -0400

    Plugin code

--- Calamity/CalamityPlugin.cs
+++ Calamity/CalamityPlugin.cs
@@ -1,6 +1,7 @@
 ﻿using Dalamud.Plugin;
 using System.Net;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Calamity
@@ -20,19 +21,44 @@ namespace Calamity
             StartServer();
         }
 
-        public void Dispose()
-        {
-            StopServer();
-        }
+        public void Dispose() => StopServer();
 
         #region HttpServer
 
         private readonly string[] PREFIXES = new string[] { "localhost", "127.0.0.1" };
         private readonly int PORT = 37435;
         private readonly string UNLOAD_DALAMUD_PATH = "/unload_dalamud";
-        private volatile bool shutdownFlag = false;
+        private readonly CancellationTokenSource tokenSource = new CancellationTokenSource();
 
         private HttpListener Listener { get; } = new HttpListener();
+
+        private void ListenerLoop(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var context = Listener.GetContext();
+                if (context.Request.Url.PathAndQuery == UNLOAD_DALAMUD_PATH)
+                {
+                    context.Response.StatusCode = 200;
+                    context.Response.ContentLength64 = 0;
+                    context.Response.OutputStream.Close();
+
+                    StopServer();
+
+                    PluginLog.Information($"Unloading dalamud");
+                    dalamud.Unload();
+
+                    break;  // If it can even get this 
+                }
+                else
+                {
+                    context.Response.StatusCode = 404;
+                    context.Response.ContentLength64 = 0;
+                    context.Response.OutputStream.Close();
+                }
+            }
+            Listener.Stop();
+        }
 
         private void StartServer()
         {
@@ -56,30 +82,7 @@ namespace Calamity
                 return;
             }
 
-            Task.Factory.StartNew(() =>
-            {
-                while (!shutdownFlag)
-                {
-                    var context = Listener.GetContext();
-                    if (context.Request.Url.PathAndQuery == UNLOAD_DALAMUD_PATH)
-                    {
-                        context.Response.StatusCode = 200;
-                        context.Response.ContentLength64 = 0;
-                        context.Response.OutputStream.Close();
-
-                        PluginLog.Information($"Unloading dalamud");
-                        
-                        dalamud.Unload();
-                        shutdownFlag = true;
-                    }
-                    else
-                    {
-                        context.Response.StatusCode = 404;
-                        context.Response.ContentLength64 = 0;
-                        context.Response.OutputStream.Close();
-                    }
-                }
-            });
+            Task.Factory.StartNew(() => ListenerLoop(tokenSource.Token));
 
             PluginLog.Information("The Seventh Umbral Calamity looms");
         }
@@ -87,13 +90,8 @@ namespace Calamity
         public void StopServer()
         {
             if (!Listener.IsListening)
-            {
-                PluginLog.Warning($"Listener is already stopped");
                 return;
-            }
-
-            shutdownFlag = true;
-            Listener.Stop();
+            tokenSource.Cancel();
         }
 
         #endregion
